3c75d5dece71dba2b3df20179aa55180e7b5a598
Maximilian Meinhardt <maximilian.meinhardt@stud.h-da.de>
2025-03-23 19:48:13 +0100

    feat: first GPU collision / weak point test

---
+++ Assets/_Project/Scripts/Simulation/Collisions/WeakPointManager.cs
@@ -0,0 +1,119 @@
+using System;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace Beakstorm.Simulation.Collisions
+{
+    [DefaultExecutionOrder(-100)]
+    public class WeakPointManager : MonoBehaviour
+    {
+        [SerializeField] private ComputeShader compute;
+        [SerializeField] private Transform damageDealer;
+        
+        public static WeakPointManager Instance;
+
+        public List<WeakPoint> WeakPoints = new List<WeakPoint>(16);
+        public GraphicsBuffer WeakPointBuffer;
+        public GraphicsBuffer DamageBuffer;
+        
+        private Vector4[] _weakPointPositions = new Vector4[16];
+        private int _bufferSize = 16;
+
+        private AsyncGPUReadbackRequest _request;
+        private NativeArray<int> _damageArray;
+
+        private void Awake()
+        {
+            Instance = this;
+            WeakPointBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, _bufferSize, sizeof(float) * 4);
+            DamageBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, _bufferSize, sizeof(int) * 1);
+            
+            _damageArray = new NativeArray<int>(_bufferSize, Allocator.Persistent);
+        }
+
+        private void OnDestroy()
+        {
+            WeakPointBuffer?.Dispose();
+            DamageBuffer?.Dispose();
+        }
+
+        private void Update()
+        {
+            UpdateBufferSize();
+            UpdatePositions();
+            
+            RequestDamageValues();
+            Collide(damageDealer.position);
+        }
+
+        private void UpdatePositions()
+        {
+            for (int i = 0; i < WeakPoints.Count; i++)
+            {
+                _weakPointPositions[i] = WeakPoints[i].PositionRadius;
+            }
+            WeakPointBuffer.SetData(_weakPointPositions);
+        }
+
+
+        private void RequestDamageValues()
+        {
+            if (_request.done)
+            {
+                if (!_request.hasError)
+                {
+                    for (int i = 0; i < WeakPoints.Count; i++)
+                    {
+                        int damage = _damageArray[i];
+                        if (damage > 0)
+                        {
+                            WeakPoints[i].ApplyDamage(damage);
+                            FlushDamage(i, damage);
+                        }
+                    }
+                }
+                _request = AsyncGPUReadback.RequestIntoNativeArray(ref _damageArray, DamageBuffer);
+            }
+        }
+        
+        private void UpdateBufferSize()
+        {
+            if (WeakPoints.Count > _bufferSize)
+            {
+                _bufferSize *= 2;
+                WeakPointBuffer.Dispose();
+                DamageBuffer.Dispose();
+                WeakPointBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, _bufferSize, sizeof(float) * 4);
+                DamageBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, _bufferSize, sizeof(int) * 1);
+
+                _damageArray = new NativeArray<int>(_bufferSize, Allocator.Persistent);
+                _weakPointPositions = new Vector4[_bufferSize];
+            }
+        }
+
+        private void FlushDamage(int i, int damage)
+        {
+            int kernel = compute.FindKernel("FlushDamage");
+            compute.SetBuffer(kernel, "_DamageBuffer", DamageBuffer);
+            compute.SetInt("_Index", i);
+            compute.SetInt("_DamageDealt", damage);
+            
+            compute.Dispatch(kernel, 1, 1, 1);
+        }
+        
+        private void Collide(Vector3 pos)
+        {
+            int kernel = compute.FindKernel("Collide");
+            
+            compute.SetBuffer(kernel, "_WeakPointPositions", WeakPointBuffer);
+            compute.SetBuffer(kernel, "_DamageBuffer", DamageBuffer);
+
+            compute.SetVector("_Position", pos);
+            compute.SetInt("_Count", WeakPoints.Count);
+            
+            compute.Dispatch(kernel, 1, 1, 1);
+        }
+    }
+}
