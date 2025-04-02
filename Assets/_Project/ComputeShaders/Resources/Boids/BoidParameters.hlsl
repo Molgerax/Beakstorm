3e6998d09a1ea042ef0da9707321b33440622de4
Maximilian Meinhardt <maximilian.meinhardt@stud.h-da.de>
2025-04-02 19:41:10 +0200

    refactor: moved all parameters to BoidParameters.hlsl

--- Assets/_Project/ComputeShaders/Resources/Boids/BoidParameters.hlsl
+++ Assets/_Project/ComputeShaders/Resources/Boids/BoidParameters.hlsl
@@ -1,10 +1,25 @@
 #ifndef _INCLUDE_BOID_PARAMETERS_
 #define _INCLUDE_BOID_PARAMETERS_
 
-// Spawning
+
+uint _NumBoids;
+
+float _Time;
+float _DeltaTime;
+
 float3      _WorldPos;
 float4x4    _WorldMatrix;
 
-// 
+RWStructuredBuffer<float3> _BoidPositionBuffer;
+RWStructuredBuffer<float3> _BoidOldPositionBuffer;
+RWStructuredBuffer<float3> _BoidVelocityBuffer;
+RWStructuredBuffer<float3> _BoidNormalBuffer;
+RWStructuredBuffer<float4> _BoidDataBuffer;
+
+
+float3 _SimulationSpace;
+float _HashCellSize;
+
+SamplerState sampler_linear_clamp;
 
 #endif
