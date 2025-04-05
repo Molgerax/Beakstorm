15b93fe41210701fd18d7d0e3adaa0670e71c927
Maximilian Meinhardt <maximilian.meinhardt@stud.h-da.de>
2025-04-05 16:41:06 +0200

    feat: added ComputeShader support for different kinds of SDF (WIP!)

--- Assets/_Project/Scripts/Simulation/Collisions/SDF/Shapes/SdfBox.cs
+++ Assets/_Project/Scripts/Simulation/Collisions/SDF/Shapes/SdfBox.cs
@@ -5,25 +5,69 @@ namespace Beakstorm.Simulation.Collisions.SDF.Shapes
 {
     public class SdfBox : AbstractSdfShape
     {
-        [SerializeField] private float3 scale;
+        [SerializeField] private float3 scale = new float3(1, 1, 1);
+
+        private float3 AdjustedScale()
+        {
+            float4x4 m = T.localToWorldMatrix;
+            float3 s = new float3(math.length(m.c0.xyz), math.length(m.c1.xyz), math.length(m.c2.xyz));
+            return s * scale;
+        }
 
         private void Update()
         {
             float3 pos = T.position;
-            _boundsMin = pos - scale;
-            _boundsMax = pos + scale;
+            float4x4 m = T.worldToLocalMatrix;
+            float3x3 s = new float3x3(m.c0.xyz, m.c1.xyz, m.c2.xyz);
             
-            float3 x = new float3(scale.x, 0, 0);
-            float3 y = new float3(0, scale.y, 0);
-            float3 z = new float3(0, 0, scale.z);
-            
-            _sdfData = new AbstractSdfData(x, y, z, pos, SdfShapeType.Box);
+            s = math.transpose(s);
+
+            CalculateBounds(pos);
+
+
+            float3 x = math.normalize(s.c0);
+            float3 y = math.normalize(s.c1);
+            float3 z = math.normalize(s.c2);
+
+            _sdfData = new AbstractSdfData(x, y, z, pos, AdjustedScale() * 0.5f, SdfShapeType.Box);
         }
-        
+
+        private void CalculateBounds(float3 center)
+        {
+            float4x4 m = T.localToWorldMatrix;
+            float3x3 rot = new float3x3(m.c0.xyz, m.c1.xyz, m.c2.xyz);
+
+            float3 adjustedScale = AdjustedScale() * 0.5f;
+            BoundingBox bounds = new BoundingBox(center - adjustedScale, center + adjustedScale);
+
+            for (int i = 0; i < 8; i++)
+            {
+                float3 p = math.mul(rot, Corners[i] * scale * 0.5f) + center;
+                bounds.GrowToInclude(p, p);
+            }
+
+            _boundsMin = bounds.Min;
+            _boundsMax = bounds.Max;
+        }
+
         public void OnDrawGizmos()
         {
             Gizmos.color = new(1, 0, 0, 0.5f);
-            Gizmos.DrawWireCube(T.position, scale);
+            Gizmos.matrix = T.localToWorldMatrix;
+            Gizmos.DrawWireCube(Vector3.zero, scale);
         }
+        
+        
+        private static readonly float3[] Corners = new float3[8]
+        {
+            new(-1, -1, -1),
+            new(+1, -1, -1),
+            new(+1, +1, -1),
+            new(+1, +1, +1),
+            new(-1, +1, +1),
+            new(-1, -1, +1),
+            new(-1, +1, -1),
+            new(+1, -1, +1),
+        };
     }
 }
