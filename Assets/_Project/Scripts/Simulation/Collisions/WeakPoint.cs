5e1b8606503cd6abec80a734a0cfca3377b667db
Molgerax <molgerax.productions@gmail.com>
2025-04-15 15:13:15 +0200

    feat: debug viz for weakpoint health

--- Assets/_Project/Scripts/Simulation/Collisions/WeakPoint.cs
+++ Assets/_Project/Scripts/Simulation/Collisions/WeakPoint.cs
@@ -16,11 +16,17 @@ namespace Beakstorm.Simulation.Collisions
         public int CurrentHealth => currentHealth;
         public float CurrentHealth01 => (float) currentHealth / maxHealth;
         public bool IsDestroyed => currentHealth <= 0;
+
+        private MeshRenderer _renderer;
+        private MaterialPropertyBlock _propBlock;
         
         private void OnEnable()
         {
             currentHealth = maxHealth;
             Subscribe();
+            
+            _propBlock ??= new MaterialPropertyBlock();
+            _renderer = GetComponent<MeshRenderer>();
         }
 
         private void OnDisable()
@@ -43,6 +49,14 @@ namespace Beakstorm.Simulation.Collisions
         public void ApplyDamage(int value)
         {
             currentHealth -= value;
+
+            if (_renderer)
+            {
+                _renderer.GetPropertyBlock(_propBlock);
+                _propBlock.SetColor("_BaseColor", new Color(CurrentHealth01, 0, 0, 1));
+                _renderer.SetPropertyBlock(_propBlock);
+            }
+            
             if (currentHealth <= 0)
                 HealthZero();
         }
