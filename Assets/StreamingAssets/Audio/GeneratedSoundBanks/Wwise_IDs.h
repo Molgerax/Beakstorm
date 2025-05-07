34eca93be6b82aa7a2a8683fd398f32df0e4c8f6
Ruben <rrlabes@gmail.com>
2025-05-07 16:41:56 +0200

    [Wwise Added]
    -started Event Catalogue
    -created GameSyncs

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,84 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_ATMO = 3133186549U;
+        static const AkUniqueID PLAY_BIRDATTACK = 2435460859U;
+        static const AkUniqueID PLAY_BIRDFOLLOW = 217586636U;
+        static const AkUniqueID PLAY_BIRDSTATIC = 4227130709U;
+        static const AkUniqueID PLAY_BIRDTARGET = 1602214372U;
+        static const AkUniqueID PLAY_GLIDERHUM = 1912775917U;
+        static const AkUniqueID PLAY_MUSIC = 2932040671U;
+        static const AkUniqueID PLAY_SHIPCANNON = 275388915U;
+        static const AkUniqueID PLAY_SHIPDESTROY = 792515034U;
+        static const AkUniqueID PLAY_SHIPDRONE = 2650593202U;
+        static const AkUniqueID PLAY_SHIPHORN = 463795711U;
+        static const AkUniqueID STOP_ALL = 452547817U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace WAVE_STATE
+        {
+            static const AkUniqueID GROUP = 108563492U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID REST = 2277599019U;
+                static const AkUniqueID WAVE1 = 2453122375U;
+                static const AkUniqueID WAVE2 = 2453122372U;
+                static const AkUniqueID WAVE3 = 2453122373U;
+                static const AkUniqueID WAVE4 = 2453122370U;
+                static const AkUniqueID WAVE5 = 2453122371U;
+            } // namespace STATE
+        } // namespace WAVE_STATE
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID PLAYER_HEALTH = 215992295U;
+        static const AkUniqueID PLAYER_SPEED = 1062779386U;
+    } // namespace GAME_PARAMETERS
+
+    namespace TRIGGERS
+    {
+        static const AkUniqueID SHIP_SINK = 2111840295U;
+        static const AkUniqueID SHIP_SPAWN = 953668639U;
+    } // namespace TRIGGERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MASTER = 4056684167U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID _2D_SFX = 1191978290U;
+        static const AkUniqueID _3D_MUSIC = 2352922065U;
+        static const AkUniqueID _3D_SFX = 3938878469U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
