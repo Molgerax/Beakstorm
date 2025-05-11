076bccf0a290b8bd58c51f36fb90fbf8ca87f23f
Molgerax <molgerax.productions@gmail.com>
2025-05-11 18:49:53 +0200

    feat: add prefab preview for enemy spawners

--- Assets/_Project/Scripts/Runtime/Gameplay/Enemies/EnemySpawner.cs
+++ Assets/_Project/Scripts/Runtime/Gameplay/Enemies/EnemySpawner.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 namespace Beakstorm.Gameplay.Enemies
 {
+    [ExecuteInEditMode]
     public class EnemySpawner : MonoBehaviour
     {
         [SerializeField] private EnemyController enemyPrefab;
@@ -14,7 +15,7 @@ namespace Beakstorm.Gameplay.Enemies
         [SerializeField] private float spawnDelay;
 
         public bool IsDefeated;
-        
+
         private EnemyController _enemy;
 
         private AwaitableCompletionSource _completionSource = new AwaitableCompletionSource();
@@ -23,12 +24,12 @@ namespace Beakstorm.Gameplay.Enemies
         {
             if (IsDefeated)
                 return;
-            
+
             _enemy = Instantiate(enemyPrefab, transform.position, transform.rotation);
             _enemy.Spawn(this);
             _completionSource.Reset();
             IsDefeated = false;
-            
+
             onSpawnEnemy?.Invoke();
         }
 
@@ -51,6 +52,7 @@ namespace Beakstorm.Gameplay.Enemies
             {
                 return _completionSource.Awaitable;
             }
+
             return Awaitable.EndOfFrameAsync();
         }
 
@@ -60,6 +62,34 @@ namespace Beakstorm.Gameplay.Enemies
             Null = 0,
             WaitForDelay = 1,
             WaitUntilDefeated = 2
+        }
+
+        private EnemyController _preview;
+
+        private void OnEnable()
+        {
+            if (!_preview)
+            {
+                _preview = Instantiate(enemyPrefab, transform.position, transform.rotation, transform);
+                UnityEditor.SceneVisibilityManager.instance.DisablePicking(_preview.gameObject, true);
+
+                HideAndDontSaveRecursive(_preview.transform);
+            }
+        }
+
+        private void HideAndDontSaveRecursive(Transform t)
+        {
+            t.gameObject.hideFlags = HideFlags.HideAndDontSave | HideFlags.HideInInspector;
+
+            for (int i = 0; i < t.childCount; i++)
+            {
+                HideAndDontSaveRecursive(t.GetChild(i));
+            }
+        }
+
+        private void OnDisable()
+        {
+            DestroyImmediate(_preview);
         }
     }
 }
