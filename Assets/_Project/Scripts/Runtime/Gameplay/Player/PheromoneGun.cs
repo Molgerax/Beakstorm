bd91cbc9a188b5d94ad0ae5e356f5396595fb842
Molgerax <molgerax.productions@gmail.com>
2025-05-26 12:54:25 +0200

    feat: added player tag, changed weapon pointing

--- Assets/_Project/Scripts/Runtime/Gameplay/Player/PheromoneGun.cs
+++ Assets/_Project/Scripts/Runtime/Gameplay/Player/PheromoneGun.cs
@@ -1,3 +1,4 @@
+using System;
 using Beakstorm.Inputs;
 using Beakstorm.Simulation.Particles;
 using UnityEngine;
@@ -7,12 +8,17 @@ namespace Beakstorm.Gameplay.Player
 {
     public class PheromoneGun : MonoBehaviour
     {
-        private PlayerInputs _inputs;
+        [SerializeField, Range(0, 5f)] private float shootOffset = 3f;
+        [SerializeField] private LayerMask layerMask;
         
+        private PlayerInputs _inputs;
+        private Camera _camera;
+
         private void Awake()
         {
             _inputs = PlayerInputs.Instance;
-
+            _camera = Camera.main;
+            
             _inputs.shootAction.performed += OnShootActionPerformed;
         }
 
@@ -28,8 +34,25 @@ namespace Beakstorm.Gameplay.Player
         {
             if (!PlayerController.Instance || !PlayerController.Instance.SelectedWeapon)
                 return;
+
+            Vector3 pos = transform.position;
+            Vector3 dir = GetShootDirection(pos);
+
+            pos += dir * shootOffset;
             
-            PlayerController.Instance.SelectedWeapon.Fire(transform.position, transform.forward);
+            PlayerController.Instance.SelectedWeapon.Fire(pos, dir);
+        }
+
+        private Vector3 GetShootDirection(Vector3 shootPosition)
+        {
+            if (!_camera) return transform.forward;
+            
+            if (Physics.Raycast(_camera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0)), out RaycastHit hit, Single.MaxValue, layerMask))
+            {
+                return (hit.point - shootPosition).normalized;
+            }
+
+            return _camera.transform.forward;
         }
     }
 }
