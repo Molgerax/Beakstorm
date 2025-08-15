d6144dc8f0bd93960f8d6971b420990d15b7b9fe
Molgerax <molgerax.productions@gmail.com>
2025-08-15 15:35:16 +0200

    feat: added encounter list for testing

---
+++ Assets/_Project/Scripts/Runtime/Gameplay/Encounters/Procedural/EncounterList.cs
@@ -0,0 +1,77 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace Beakstorm.Gameplay.Encounters.Procedural
+{
+    public class EncounterList : MonoBehaviour
+    {
+        [SerializeField] private WaveDescription[] waves;
+
+        [SerializeField] private int currentIndex;
+        
+        private UniTask _task;
+        private CancellationTokenSource _tokenSource;
+
+        private void Awake()
+        {
+            _tokenSource = new();
+        }
+
+        private void OnDestroy()
+        {
+            _tokenSource?.Cancel();
+            _tokenSource?.Dispose();
+        }
+        
+
+        public void StartEncounter()
+        {
+            _task = WaveLoop(_tokenSource.Token);
+        }
+
+
+        private async UniTask WaveLoop(CancellationToken token)
+        {
+            for (var index = 0; index < waves.Length; index++)
+            {
+                currentIndex = index;
+                
+                WaveDescription wave = waves[index];
+                if (wave.WaveData == null)
+                {
+                    EncounterManager.Instance.SetPeace(wave.CalmIndex);
+                    await UniTask.Delay(wave.CalmDuration * 1000, cancellationToken: token);
+                }
+                else
+                {
+                    EncounterManager.Instance.SetWar(wave.CalmIndex);
+                    EncounterManager.Instance.BeginWave(wave.WaveData);
+
+                    while (EncounterManager.Instance.IsWaveActive)
+                    {
+                        token.ThrowIfCancellationRequested();
+                        await UniTask.Yield(token);
+                    }
+                }
+            }
+        }
+
+
+
+        [System.Serializable]
+        public struct WaveDescription
+        {
+            public WaveDataSO WaveData;
+            public int CalmIndex;
+            public int CalmDuration;
+
+            public WaveDescription(WaveDataSO data)
+            {
+                WaveData = data;
+                CalmIndex = 1;
+                CalmDuration = 15;
+            }
+        }
+    }
+}
