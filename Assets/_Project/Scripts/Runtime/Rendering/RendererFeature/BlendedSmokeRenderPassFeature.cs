815d9a5ac3a2ff415fd764eb75a1e13cfa9736a8
Molgerax <molgerax.productions@gmail.com>
2025-09-17 17:24:58 +0200

    feat: added blended smoke render feature (WIP)

---
+++ Assets/_Project/Scripts/Runtime/Rendering/RendererFeature/BlendedSmokeRenderPassFeature.cs
@@ -0,0 +1,227 @@
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.RenderGraphModule;
+using UnityEngine.Rendering.RenderGraphModule.Util;
+using UnityEngine.Rendering.Universal;
+
+namespace Beakstorm.Rendering.RendererFeature
+{
+    public class BlendedSmokeRenderPassFeature : ScriptableRendererFeature
+    {
+        class BlendedSmokeRenderPass : ScriptableRenderPass
+        {
+            private const string k_PassName = "BlendedSmokePass";
+            
+            private Material _blitMaterial;
+            private int _layerMask;
+            private float _blendStrength;
+            private int _blurSize;
+            private float _blurSigma;
+
+            private ShaderTagId _forwardTag = new ShaderTagId("SRPDefaultUnlit");
+            private ShaderTagId _stencilTag = new ShaderTagId("UniversalForwardStencil");
+            
+
+            // This class stores the data needed by the RenderGraph pass.
+            // It is passed as a parameter to the delegate function that executes the RenderGraph pass.
+            private class PassData
+            {
+                public UniversalCameraData CameraData;
+                public RendererListHandle RendererListHdl;
+                public TextureHandle LowResDepth;
+                public TextureHandle Edges;
+                public int DownSample;
+            }
+
+            public void Setup(Material blitMaterial, int layerMask, float blendStrength, int blurSize, float blurSigma)
+            {
+                _blitMaterial = blitMaterial;
+                _layerMask = layerMask;
+                _blendStrength = blendStrength;
+                _blurSize = blurSize;
+                _blurSigma = blurSigma;
+            }
+
+            private void InitRendererLists(ShaderTagId tagId, UniversalRenderingData renderingData, UniversalLightData lightData,
+                ref PassData passData, ScriptableRenderContext context, RenderGraph renderGraph)
+            {
+                SortingCriteria sortingCriteria = passData.CameraData.defaultOpaqueSortFlags;
+                DrawingSettings drawingSettings = RenderingUtils.CreateDrawingSettings(tagId, renderingData, passData.CameraData, lightData, sortingCriteria);
+
+                FilteringSettings filteringSettings = FilteringSettings.defaultValue;
+                filteringSettings.layerMask = _layerMask;
+                filteringSettings.renderQueueRange = RenderQueueRange.opaque;
+
+                RendererListParams listParams = new RendererListParams(renderingData.cullResults, drawingSettings,
+                    filteringSettings);
+                listParams.tagName = tagId;
+                
+                passData.RendererListHdl = renderGraph.CreateRendererList(listParams);
+            }
+
+            // This static method is passed as the RenderFunc delegate to the RenderGraph render pass.
+            // It is used to execute draw commands.
+            static void ExecutePass(PassData data, RasterGraphContext context)
+            {
+                Rect pixelRect = data.CameraData.camera.pixelRect;
+                float renderScale = data.CameraData.isSceneViewCamera ? 1f : data.CameraData.renderScale;
+                float scaledCameraWidth = (float)pixelRect.width * renderScale / data.DownSample;
+                float scaledCameraHeight = (float)pixelRect.height * renderScale / data.DownSample;
+
+                //context.cmd.SetGlobalVector("_ScaledScreenParams", new Vector4(scaledCameraWidth, scaledCameraHeight, 1.0f + 1.0f / scaledCameraWidth, 1.0f + 1.0f / scaledCameraHeight));
+                
+                context.cmd.DrawRendererList(data.RendererListHdl);
+
+                //context.cmd.SetGlobalVector("_ScaledScreenParams", new Vector4(scaledCameraWidth, scaledCameraHeight, 1.0f + 1.0f / scaledCameraWidth, 1.0f + 1.0f / scaledCameraHeight));
+            }
+            
+            private class BlitPassData
+            {
+                public TextureHandle Source;
+                public int PassIndex;
+                public Material Material;
+            }
+            
+            static void ExecuteBlitPass(BlitPassData data, RasterGraphContext context)
+            {
+                Blitter.BlitTexture( context.cmd, data.Source, new Vector4( 1, 1, 0, 0 ), data.Material, data.PassIndex );
+            }
+            
+            void AddBlitPass( RenderGraph renderGraph, TextureHandle source, TextureHandle destination, TextureHandle depth, Material material, string passName, int passIndex, TextureHandle edges )
+            {
+                using (var builder = renderGraph.AddRasterRenderPass(passName, out BlitPassData passData))
+                {
+                    builder.UseTexture(source);
+                    passData.Source = source;
+                    passData.PassIndex = passIndex;
+                    passData.Material = material;
+                    
+                    builder.UseTexture(edges);
+
+                    builder.SetRenderAttachment(destination, 0);
+                    builder.SetRenderAttachmentDepth(depth, AccessFlags.ReadWrite);
+                    builder.SetRenderFunc<BlitPassData>((data, context) =>
+                    {
+                        ExecuteBlitPass(data, context);
+                    });
+                }
+            }
+            
+            public class TextureBindInfo
+            {
+                public int slot;
+                public TextureHandle texture;
+            }
+
+
+            // RecordRenderGraph is where the RenderGraph handle can be accessed, through which render passes can be added to the graph.
+            // FrameData is a context container through which URP resources can be accessed and managed.
+            public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData)
+            {
+                if (!_blitMaterial)
+                    return;
+            
+                UniversalCameraData cameraData = frameData.Get<UniversalCameraData>();
+                UniversalRenderingData renderingData = frameData.Get<UniversalRenderingData>();
+                UniversalLightData lightData = frameData.Get<UniversalLightData>();
+                UniversalResourceData resourceData = frameData.Get<UniversalResourceData>();
+                
+                RenderTextureDescriptor desc = cameraData.cameraTargetDescriptor;
+                RenderTextureDescriptor descFull = desc;
+                descFull.depthStencilFormat = GraphicsFormat.None;
+                
+                TextureHandle destinationFullRes =
+                    UniversalRenderer.CreateRenderGraphTexture(renderGraph, descFull, "Destination", true);
+                
+                TextureHandle destinationBack =
+                    UniversalRenderer.CreateRenderGraphTexture(renderGraph, descFull, "DestinationBack", true);
+                                
+                RenderGraphUtils.BlitMaterialParameters blit =
+                    new RenderGraphUtils.BlitMaterialParameters(destinationBack, destinationFullRes, _blitMaterial, 0);
+                renderGraph.AddBlitPass(blit);
+                
+                _blitMaterial.SetFloat("_BlendStrength", _blendStrength);
+                _blitMaterial.SetFloat("_Sigma", _blurSigma);
+                _blitMaterial.SetInteger("_BlurSize", _blurSize);
+                
+                // This adds a raster render pass to the graph, specifying the name and the data type that will be passed to the ExecutePass function.
+                using (var builder = renderGraph.AddRasterRenderPass<PassData>(k_PassName, out var passData))
+                {
+                    passData.CameraData = cameraData;
+
+                    InitRendererLists(_forwardTag, renderingData, lightData, ref passData, default, renderGraph);
+                    builder.UseRendererList(passData.RendererListHdl);
+                    builder.AllowPassCulling(false);
+                    builder.AllowGlobalStateModification(true);
+
+                    
+                    // This sets the render target of the pass to the active color texture. Change it to your own render target as needed.
+                    //builder.SetRenderAttachment(resourceData.activeColorTexture, 0);
+                    builder.SetRenderAttachment(destinationFullRes, 0, AccessFlags.ReadWrite);
+                    builder.SetRenderAttachmentDepth(resourceData.cameraDepth);
+                    
+                    // Assigns the ExecutePass function to the render pass delegate. This will be called by the render graph when executing the pass.
+                    builder.SetRenderFunc((PassData data, RasterGraphContext context) =>
+                    {
+                        ExecutePass(data, context);
+                    });
+
+                    //resourceData.cameraColor = destination;
+                }
+
+                
+                //RenderGraphUtils.BlitMaterialParameters edgeBlit = 
+                //    new RenderGraphUtils.BlitMaterialParameters(destinationFullRes, , _blitMaterial, 2);
+                //renderGraph.AddBlitPass(edgeBlit);
+                
+                blit =
+                    new RenderGraphUtils.BlitMaterialParameters(destinationFullRes, destinationBack, _blitMaterial, 1);
+                renderGraph.AddBlitPass(blit);
+                
+                blit =
+                    new RenderGraphUtils.BlitMaterialParameters(destinationBack, destinationFullRes, _blitMaterial, 2);
+                renderGraph.AddBlitPass(blit);
+
+                blit = 
+                    new RenderGraphUtils.BlitMaterialParameters(destinationFullRes, resourceData.cameraColor, _blitMaterial, 3);
+                renderGraph.AddBlitPass(blit);
+            }
+        }
+        
+        [SerializeField] private LayerMask layerMask;
+
+        [SerializeField] private Material blitMaterial;
+        
+        [SerializeField, Range(0, 1f)] 
+        private float blendStrength = 1;
+
+        [SerializeField, Range(0, 8)] 
+        private int blurSize = 4;
+        
+        [SerializeField, Range(0, 5f)] 
+        private float blurSigma = 1;
+        
+        
+        BlendedSmokeRenderPass m_ScriptablePass;
+
+        /// <inheritdoc/>
+        public override void Create()
+        {
+            m_ScriptablePass = new BlendedSmokeRenderPass();
+
+            // Configures where the render pass should be injected.
+            m_ScriptablePass.renderPassEvent = RenderPassEvent.AfterRenderingTransparents;
+        }
+
+        // Here you can inject one or multiple render passes in the renderer.
+        // This method is called when setting up the renderer once per-camera.
+        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+        {
+            float sigma = (blurSize * 2 + 1) / (6f * Mathf.Max(0.001f, blurSigma));
+            
+            m_ScriptablePass.Setup(blitMaterial, layerMask, blendStrength, blurSize, sigma);
+            renderer.EnqueuePass(m_ScriptablePass);
+        }
+    }
+}
---
+++ Assets/_Project/Shaders/Resources/Rendering/Smoke/BlendedSmokeBlit.shader
@@ -0,0 +1,256 @@
+Shader "Beakstorm/BlendedSmoke/Blit"
+{
+
+    HLSLINCLUDE
+
+    #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+    #include "Packages/com.unity.render-pipelines.core/Runtime/Utilities/Blit.hlsl"
+
+
+    SAMPLER(sampler_BlitTexture);
+
+    
+    float _BlendStrength;
+    float4 _TargetSize;
+
+
+    inline float4 ApplyBlendStrength(float4 col)
+    {
+        return col * _BlendStrength;
+    }
+
+    float GaussianBlurWeight(int x, int y, float sigma)
+    {
+        int sqrDist = x * x + y * y;
+        float c = 2 * sigma * sigma;
+        return exp(-sqrDist / c);
+    }
+    
+    float4 GaussianBlur1D(float2 uv, float2 dir, int blurSize, float sigma)
+    {
+        float4 sum = 0;
+        float weightSum = 0;
+        float2 texelDelta = _BlitTexture_TexelSize.xy * dir;
+
+        float4 middleSample = 0;
+        
+        for (int x = -blurSize; x <= blurSize; x++)
+        {
+            float2 uv2 = uv + texelDelta * x;
+            float4 sample = SAMPLE_TEXTURE2D(_BlitTexture, sampler_BlitTexture, uv2);
+            float depth = sample.r;
+
+            
+            if (x == 0)
+                middleSample = sample;
+            
+            if (depth < 10000)
+            {
+                float weight = GaussianBlurWeight(x, 0, sigma);
+                weightSum += weight;
+                sum += sample * weight;
+            }
+        }
+
+        if (weightSum == 0)
+            return 0;
+
+        if (middleSample.r > 10000)
+            return middleSample;
+        
+        return sum / weightSum;
+    }
+    
+    ENDHLSL
+    
+    SubShader
+    {
+        Tags { "RenderType" = "Opaque" "RenderPipeline" = "UniversalPipeline"}
+        LOD 100
+
+        Pass
+        {
+            Name "Blit"
+            ZTest Always
+            ZWrite Off
+            Cull Off
+            
+            //Blend SrcAlpha OneMinusSrcAlpha, Zero OneMinusSrcAlpha
+            Blend One OneMinusSrcAlpha
+
+            Stencil 
+            {
+                Ref 1
+                Comp Always
+                Pass Replace
+            }    
+            
+            HLSLPROGRAM
+            #pragma vertex Vert
+            #pragma fragment Fragment
+            #pragma multi_compile_fragment _ _LINEAR_TO_SRGB_CONVERSION
+            #pragma multi_compile_fragment _ DEBUG_DISPLAY
+
+            // Core.hlsl for XR dependencies
+            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+            #include "Packages/com.unity.render-pipelines.core/Runtime/Utilities/Blit.hlsl"
+            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Debug/DebuggingFullscreen.hlsl"
+            #include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Color.hlsl"
+
+            TEXTURE2D(_EdgeTexture);
+
+            half4 Fragment(Varyings input) : SV_Target
+            {
+                float2 uv = input.texcoord;
+
+                float4 col = SAMPLE_TEXTURE2D(_BlitTexture, sampler_BlitTexture, uv);
+                float alpha = col.a;
+
+                return 1.#INF;
+            }
+            ENDHLSL
+        }
+        
+        Pass
+        {
+            Name "Gaussian Horizontal"
+            ZTest Always
+            ZWrite Off
+            Cull Off
+
+            HLSLPROGRAM
+            #pragma vertex Vert
+            #pragma fragment Fragment
+
+            float _SobelCutoff;
+            float _BlurSize;
+            float _Sigma;
+            
+            half4 Fragment(Varyings input) : SV_Target
+            {
+                return GaussianBlur1D(input.texcoord, float2(1, 0), _BlurSize, _Sigma);
+            }
+            ENDHLSL
+        }
+        
+        Pass
+        {
+            Name "Gaussian Vertical"
+            ZTest Always
+            ZWrite Off
+            Cull Off
+
+            HLSLPROGRAM
+            #pragma vertex Vert
+            #pragma fragment Fragment
+
+            float _SobelCutoff;
+            float _BlurSize;
+            float _Sigma;
+            
+            half4 Fragment(Varyings input) : SV_Target
+            {
+                //return SAMPLE_TEXTURE2D(_BlitTexture, sampler_BlitTexture, input.texcoord);
+                return GaussianBlur1D(input.texcoord, float2(0, 1), _BlurSize, _Sigma);
+            }
+            ENDHLSL
+        }
+        
+        Pass
+        {
+            Name "BlitNoEdge"
+            ZTest Always
+            ZWrite Off
+            Cull Off
+            
+            //Blend SrcAlpha OneMinusSrcAlpha, Zero OneMinusSrcAlpha
+            Blend SrcAlpha OneMinusSrcAlpha
+
+            
+            HLSLPROGRAM
+            #pragma vertex Vert
+            #pragma fragment Fragment
+            #pragma multi_compile_fragment _ _LINEAR_TO_SRGB_CONVERSION
+            #pragma multi_compile_fragment _ DEBUG_DISPLAY
+
+            // Core.hlsl for XR dependencies
+            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+            #include "Packages/com.unity.render-pipelines.core/Runtime/Utilities/Blit.hlsl"
+            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Debug/DebuggingFullscreen.hlsl"
+            #include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Color.hlsl"
+
+            
+	        #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
+	        TEXTURE2D(_CameraDepthAttachment);
+            
+            float4 ViewPos(float2 uv)
+            {
+                float depth = SAMPLE_TEXTURE2D(_BlitTexture, sampler_BlitTexture, uv).r;
+                float3 viewVector = mul(unity_CameraInvProjection, float4(uv.xy * 2 - 1, 0, 1));
+                return float4(normalize(viewVector) * depth, depth);
+            }
+
+            half4 Fragment(Varyings input) : SV_Target
+            {
+                float2 uv = input.texcoord;
+                
+                float4 col = SAMPLE_TEXTURE2D(_BlitTexture, sampler_BlitTexture, uv);
+                float alpha = col.a;
+
+                float3 viewVector = mul((float3x3)unity_CameraToWorld, float3(0,0,1));;
+                
+                float4 posCenter = ViewPos(uv);
+                if (posCenter.a > 10000)
+                {
+                    return 0;
+                }
+
+
+
+                float2 stepSize = _BlitTexture_TexelSize.xy;
+                float3 ddx = ViewPos(uv + float2(stepSize.x, 0)) - posCenter;
+                float3 ddx2 = posCenter - ViewPos(uv + float2(-stepSize.x, 0));
+                if (abs(ddx2.z) < abs(ddx.z))
+                {
+                    ddx = ddx2;
+                }
+                
+                float3 ddy = ViewPos(uv + float2(0, stepSize.y)) - posCenter;
+                float3 ddy2 = posCenter - ViewPos(uv + float2(0,-stepSize.y));
+                if (abs(ddy2.z) < abs(ddy.z)) {
+                    ddy = ddy2;
+                }
+                
+                float3 viewNormal = normalize(cross(ddy, ddx));
+                float3 worldNormal = TransformViewToWorldNormal(viewNormal);
+
+                worldNormal = normalize(col.rgb);
+                viewNormal = TransformWorldToViewNormal(worldNormal, true);
+
+                
+                Light light = GetMainLight();
+                col.rgb = worldNormal * 0.5 + 0.5;
+
+                float3 rimLightDir = normalize(cross(float3(0,1,0), light.direction));
+                
+                float rimLight = dot(-viewVector, worldNormal);
+                
+                float l = dot(light.direction, worldNormal);
+                col.rgb = lerp(0.3, 0.8, step(0, l));
+
+                col.rgb = lerp(col.rgb, 0, step(rimLight, 0.2));
+
+                
+                //#ifdef _LINEAR_TO_SRGB_CONVERSION
+                //col = LinearToSRGB(col);
+                //#endif
+
+
+                col = ApplyBlendStrength(col);
+                
+                return col;
+            }
+            ENDHLSL
+        }
+    }
+}
