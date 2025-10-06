f55744509741db813c98316baba5fc6e3154d7da
Molgerax <molgerax.productions@gmail.com>
2025-10-07 00:44:31 +0200

    feat: added Master shader with custom lighting (WIP)

---
+++ Assets/_Project/Shaders/Graphs/Subgraphs/GetMainLight.hlsl
@@ -0,0 +1,101 @@
+#ifndef SHADERGRAPH_PREVIEW
+#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
+#endif
+
+
+#ifndef SHADERGRAPH_PREVIEW
+struct Surface
+{
+    float3 normal;
+    float3 view;
+    float shininess;
+    float smoothness;
+};
+
+float3 CalculateCelShading(Light l, Surface s)
+{
+    float attenuation = l.shadowAttenuation * l.distanceAttenuation;
+    float diffuse = saturate(dot(s.normal, l.direction));
+    diffuse *= attenuation;
+    
+    float3 h = SafeNormalize(l.direction + s.view);
+    float specular = saturate(dot(s.normal, h));
+    specular = pow(specular, s.shininess);
+    specular *= diffuse * s.smoothness;
+    
+    return (diffuse + specular) * l.color;
+}
+#endif
+
+void LightingCelShaded_float(float3 Position, float2 ScreenPosition, float3 Normal, float3 View, float Smoothness, out float3 Color)
+{
+#ifndef SHADERGRAPH_PREVIEW
+    Surface s = (Surface)0;
+    s.normal = SafeNormalize(Normal);
+    s.view = SafeNormalize(View);
+    s.shininess = exp2(10 * Smoothness + 1);
+    s.smoothness = Smoothness;
+
+#if SHADOWS_SCREEN
+    float4 clipPos = TransformWorldToHClip(Position);
+    float4 shadowCoord = ComputeScreenPos(clipPos);
+#else
+    float4 shadowCoord = TransformWorldToShadowCoord(Position);
+#endif
+
+    Light light = GetMainLight(shadowCoord);
+    Color = CalculateCelShading(light, s);
+
+    int pixelLightCount = GetAdditionalLightsCount();
+    //for (int i = 0; i < pixelLightCount; i++)
+    //{
+    //    light = GetAdditionalLight(i, Position, 1);
+    //    Color += CalculateCelShading(light, s);
+    //}
+
+    #if USE_FORWARD_PLUS
+    InputData inputData = (InputData)0;
+    inputData.normalizedScreenSpaceUV = ScreenPosition;
+    inputData.positionWS = Position;
+    #endif
+
+    #if USE_FORWARD_PLUS
+    UNITY_LOOP for (uint lightIndex = 0; lightIndex < min(URP_FP_DIRECTIONAL_LIGHTS_COUNT, MAX_VISIBLE_LIGHTS); lightIndex++)
+    {
+        Light additionalLight = GetAdditionalLight(lightIndex, inputData.positionWS, half4(1,1,1,1));
+        Color += CalculateCelShading(additionalLight, s);
+    }
+    #endif
+    
+    LIGHT_LOOP_BEGIN(pixelLightCount)
+        #if !USE_FORWARD_PLUS
+            lightIndex = GetPerObjectLightIndex(lightIndex);
+        #endif
+        light = GetAdditionalLight(lightIndex, Position, 1);
+        Color += CalculateCelShading(light, s);
+    LIGHT_LOOP_END
+    
+    
+#else
+    Color = 1;
+#endif    
+}
+
+
+void MainLight_float(out float3 Direction, out float3 Color, out float DistanceAtten, out float ShadowAtten, out uint LayerMask)
+{
+#if SHADERGRAPH_PREVIEW
+    Direction = 0.5;
+    Color = 1;
+    DistanceAtten = 1;
+    ShadowAtten = 1;
+    LayerMask = 0;
+#else
+    Light light = GetMainLight();
+    Direction = light.direction;
+    Color = light.color;
+    DistanceAtten = light.distanceAttenuation;
+    ShadowAtten = light.shadowAttenuation;
+    LayerMask = light.layerMask;
+#endif
+}
