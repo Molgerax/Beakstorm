4b3db14afba36fc6cc47e397467992917e816cbd
Molgerax <molgerax.productions@gmail.com>
2025-10-15 19:01:33 +0200

    feat: added spiral pattern to multi shot

--- Assets/_Project/Scripts/Runtime/Gameplay/Player/Weapons/MultiShotPlayerWeapon.cs
+++ Assets/_Project/Scripts/Runtime/Gameplay/Player/Weapons/MultiShotPlayerWeapon.cs
@@ -7,7 +7,7 @@ namespace Beakstorm.Gameplay.Player.Weapons
     [CreateAssetMenu(menuName = "Beakstorm/PlayerWeaponData/MultiShotProjectile", fileName = "MultiShotPlayerWeapon")]
     public class MultiShotPlayerWeapon : SimplePlayerWeapon
     {
-        [SerializeField, Range(1, 15)] private int count = 1;
+        [SerializeField, Min(1)] private int count = 1;
         [SerializeField, Range(1f, 360f)] private float spreadAngle = 25f;
         [SerializeField, Range(0f, 3f)] private float offset = 1f;
         
@@ -16,6 +16,8 @@ namespace Beakstorm.Gameplay.Player.Weapons
             for (int i = 0; i < count; i++)
             {
                 Vector3 shotDirection = GetShotDirection(i, direction);
+
+                shotDirection = GetSpiralPattern(i, direction);
                 
                 base.FireSingleProjectile(position + shotDirection * offset, shotDirection);
             }
@@ -28,6 +30,30 @@ namespace Beakstorm.Gameplay.Player.Weapons
 
             float angle = GetAngle(index);
             return Quaternion.AngleAxis(angle, rotationAxis) * initDirection;
+        }
+
+        private Vector3 GetSpiralPattern(int index, Vector3 initDirection)
+        {
+            float turnFraction = 1.618033988f;
+
+            float angleIncrement = 2 * Mathf.PI * turnFraction;
+            
+            float t = index / (count - 1f);
+
+            t *= (spreadAngle / 360f);
+            
+            float inclination = Mathf.Acos(1 - 2 * t);
+            float azimuth = angleIncrement * index;
+
+            float x = Mathf.Sin(inclination) * Mathf.Cos(azimuth);
+            float y = Mathf.Sin(inclination) * Mathf.Sin(azimuth);
+            float z = Mathf.Cos(inclination);
+
+            Vector3 target = new Vector3(x, y, z);
+            Quaternion dir = Quaternion.LookRotation(target);
+            Quaternion initDir = Quaternion.LookRotation(initDirection);
+
+            return initDir * target;
         }
 
         private float GetAngle(int index)
