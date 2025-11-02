088bb8309a03fe0b4e14b5b4fedf15b032290402
Molgerax <molgerax.productions@gmail.com>
2025-11-02 17:34:18 +0100

    feat: tremble enemy spawn, now useless?

---
+++ Assets/_Project/Scripts/Runtime/Mapping/Tremble/EnemySpawnMapProcessor.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using Beakstorm.Gameplay.Encounters.Procedural;
+using TinyGoose.Tremble;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace Beakstorm.Mapping.Tremble
+{
+    public class EnemySpawnMapProcessor : MapProcessorBase
+    {
+        private List<TrembleEnemySpawn> _enemySpawns;
+        
+        public override void ProcessPrefabEntity(MapBsp mapBsp, BspEntity entity, GameObject prefab)
+        {
+            _enemySpawns ??= new();
+        
+            if (!prefab.TryGetComponent(out TrembleEnemySpawn spawn))
+                return;
+
+            _enemySpawns.Add(spawn);
+            return;
+        }
+
+        public override void OnProcessingCompleted(GameObject root, MapBsp mapBsp)
+        {
+            for (int i = _enemySpawns.Count - 1; i >= 0; i--)
+            {
+                var spawn = _enemySpawns[i];
+                StripGameObject(spawn.gameObject);
+                AttachEnemySpawnPoint(spawn, spawn.gameObject);
+                CoreUtils.Destroy(spawn);
+                _enemySpawns.RemoveAt(i);
+            }
+        }
+
+        private void StripGameObject(GameObject gameObject)
+        {
+            for (int i = gameObject.transform.childCount - 1; i >= 0; i--)
+            {
+                Transform t = gameObject.transform.GetChild(i);
+                CoreUtils.Destroy(t.gameObject);
+            }
+            var components = gameObject.GetComponents<Component>();
+            for (int i = components.Length - 1; i >= 0; i--)
+            {
+                var c = components[i];
+                
+                if(c == gameObject.GetComponent<TrembleEnemySpawn>() || c == c.transform)
+                    continue;
+                CoreUtils.Destroy(c);
+            }
+        }
+
+        private void AttachEnemySpawnPoint(TrembleEnemySpawn spawn, GameObject go)
+        {
+            EnemySpawnPoint spawnPoint = go.AddComponent<EnemySpawnPoint>();
+            spawnPoint.Init(spawn.Enemy, 0, spawn.SpawnDelay);
+
+
+            if (spawn.WaveData)
+            {
+                spawn.WaveData.AddSpawnPoint(spawnPoint);
+                return;
+            }
+
+            WaveData waveData = FindOrCreateWaveData(spawn, go);
+            if (waveData)
+                waveData.AddSpawnPoint(spawnPoint);
+        }
+        
+        private WaveData FindOrCreateWaveData(TrembleEnemySpawn spawn, GameObject go)
+        {
+            WaveData waveData = go.GetComponentInParent<WaveData>();
+            if (waveData == null)
+                waveData = go.transform.parent.gameObject.AddComponent<WaveData>();
+            return waveData;
+        }
+    }
+}
--- Assets/_Project/Scripts/Runtime/Mapping/Tremble/TrembleEnemySpawn.cs
+++ Assets/_Project/Scripts/Runtime/Mapping/Tremble/TrembleEnemySpawn.cs
@@ -6,32 +6,77 @@ using UnityEngine.Rendering;
 
 namespace Beakstorm.Mapping.Tremble
 {
-    [PrefabEntity(category:"enemy")]
-    public class TrembleEnemySpawn : MonoBehaviour, IOnImportFromMapEntity
+    //[PrefabEntity(category:"enemy")]
+    public class TrembleEnemySpawn : MonoBehaviour, IOnImportFromMapEntity, IOnLiveUpdate
     {
         [SerializeField] private int waveIndex = 0;
         [SerializeField] private float spawnDelay = 0;
 
+        [SerializeField]
         [Tremble("target")] private WaveData _waveData;
         
         [SerializeField, HideInInspector, NoTremble]
         public EnemySO Enemy;
+
+        public float SpawnDelay => spawnDelay;
+        public WaveData WaveData => _waveData;
+
+        public void OnLiveUpdated()
+        {
+            StripAndReplace();
+        }
         
         public void OnImportFromMapEntity(MapBsp mapBsp, BspEntity entity)
         {
+            StripAndReplace();
+            return;
+            
+            
             GameObject newGameObject = new GameObject(gameObject.name + "_Spawn");
             newGameObject.transform.SetParent(transform.parent);
             newGameObject.transform.SetLocalPositionAndRotation(transform.localPosition, transform.localRotation);
 
-            EnemySpawnPoint spawnPoint = newGameObject.AddComponent<EnemySpawnPoint>();
+            AttachEnemySpawnPoint(newGameObject);
+            
+            CoreUtils.Destroy(gameObject);
+        }
+
+        private void StripAndReplace()
+        {
+            StripGameObject();
+            AttachEnemySpawnPoint(gameObject);
+            CoreUtils.Destroy(this);
+        }
+
+        private void StripGameObject()
+        {
+            for (int i = transform.childCount - 1; i >= 0; i--)
+            {
+                Transform t = transform.GetChild(i);
+                CoreUtils.Destroy(t.gameObject);
+            }
+            var components = gameObject.GetComponents<Component>();
+            for (int i = components.Length - 1; i >= 0; i--)
+            {
+                var c = components[i];
+                
+                if(c == this || c == c.transform)
+                    continue;
+                CoreUtils.Destroy(c);
+            }
+        }
+
+        private EnemySpawnPoint AttachEnemySpawnPoint(GameObject go)
+        {
+            EnemySpawnPoint spawnPoint = go.AddComponent<EnemySpawnPoint>();
             spawnPoint.Init(Enemy, waveIndex, spawnDelay);
             
             
             WaveData waveData = FindOrCreateWaveData();
             if (waveData)
                 waveData.AddSpawnPoint(spawnPoint);
-            
-            CoreUtils.Destroy(gameObject);
+
+            return spawnPoint;
         }
 
         private WaveData FindOrCreateWaveData()
