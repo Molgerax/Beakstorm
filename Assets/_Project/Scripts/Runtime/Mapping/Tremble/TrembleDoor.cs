f58b34849581d0f9a5066332d465140c41fb9c75
Molgerax <molgerax.productions@gmail.com>
2025-11-05 21:07:56 +0100

    feat: door

--- Assets/_Project/Scripts/Runtime/Mapping/Tremble/TrembleDoor.cs
+++ Assets/_Project/Scripts/Runtime/Mapping/Tremble/TrembleDoor.cs
@@ -1,27 +1,71 @@
-﻿using Beakstorm.Simulation.Collisions.SDF;
-using Beakstorm.Simulation.Collisions.SDF.Shapes;
-using TinyGoose.Tremble;
+﻿using TinyGoose.Tremble;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 namespace Beakstorm.Mapping.Tremble
 {
-    [BrushEntity("box", category:"collision", type: BrushType.Solid)]
-    public class TrembleBoxCollider : MonoBehaviour, IOnImportFromMapEntity
+    [PointEntity("door", category:"func", size: 16f, colour:"0 0.5 1.0")]
+    public class TrembleDoor : TriggerBehaviour, IOnImportFromMapEntity
     {
-        [Tremble("sdfMaterial")] private SdfMaterialType _sdfMaterialType = SdfMaterialType.None;
+        [SerializeField, NoTremble] private float speed = 5f;
+        [SerializeField, NoTremble] private float distance = 16;
+
+        [Tremble("distance")] private float _trembleDistance = 64;
+        [Tremble("speed")] private float _trembleSpeed = 64;
+
+        private float _timer;
+        private float Duration => speed > 0 ? distance / speed : 0;
+
+        private bool _triggered;
+
+        private Vector3 _initPos;
+        private Vector3 _targetPos;
+
+        private DoorState _state = DoorState.Idle;
         
+        private void Awake()
+        {
+            _state = DoorState.Idle;
+            _initPos = transform.position;
+            _targetPos = _initPos + transform.right * distance;
+        }
+
+        private enum DoorState
+        {
+            Idle = 0,
+            Triggered = 1,
+            Finished = 2
+        }
+
+        public override void Trigger()
+        {
+            if (_state != DoorState.Idle)
+                return;
+            
+            _timer = 0;
+            _state = DoorState.Triggered;
+        }
+
+        private void Update()
+        {
+            if (_state != DoorState.Triggered)
+                return;
+
+            _timer += Time.deltaTime;
+            
+            if (_timer > Duration)
+            {
+                _state = DoorState.Finished;
+                transform.position = _targetPos;
+                return;
+            }
+            
+            transform.position = Vector3.Lerp(_initPos, _targetPos, _timer / Duration);
+        }
+
         public void OnImportFromMapEntity(MapBsp mapBsp, BspEntity entity)
         {
-            MeshCollider meshCollider = GetComponent<MeshCollider>();
-            Bounds bounds = meshCollider.bounds;
-            
-            BoxCollider boxCollider = gameObject.AddComponent<BoxCollider>();
-            CoreUtils.Destroy(meshCollider);
-
-            SdfBox sdfBox = gameObject.AddComponent<SdfBox>();
-            sdfBox.SetDimensions(boxCollider.center, boxCollider.size);
-            sdfBox.SetMaterialType(_sdfMaterialType);
+            distance = (_trembleDistance * TrembleSyncSettings.Get().ImportScale);
+            speed = (_trembleSpeed * TrembleSyncSettings.Get().ImportScale);
         }
     }
 }
