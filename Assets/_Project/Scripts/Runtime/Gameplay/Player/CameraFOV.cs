cf51559c7d2e7fb3234fdfec9c3021edf878e627
Molgerax <molgerax.productions@gmail.com>
2025-11-13 13:02:04 +0100

    feat: camera fov also includes camera distance

--- Assets/_Project/Scripts/Runtime/Gameplay/Player/CameraFOV.cs
+++ Assets/_Project/Scripts/Runtime/Gameplay/Player/CameraFOV.cs
@@ -8,7 +8,12 @@ namespace Beakstorm.Gameplay.Player
         [SerializeField] private float min = 60;
         [SerializeField] private float max = 70;
 
+        [SerializeField] private AnimationCurve curve = AnimationCurve.Linear(0, 0, 1, 1);
+        
         [SerializeField] private CinemachineCamera cam;
+        [SerializeField] private CinemachinePositionComposer composer;
+
+        [SerializeField] private Vector2 distances = new(6, 10);
         
         public static CameraFOV Instance;
 
@@ -23,7 +28,15 @@ namespace Beakstorm.Gameplay.Player
             if (!cam)
                 return;
 
-            cam.Lens.FieldOfView = Mathf.Lerp(min, max, value01);
+            cam.Lens.FieldOfView = Mathf.Lerp(min, max, curve.Evaluate(value01));
+        }
+
+        public void SetCameraDistance(float value01)
+        {
+            if (!composer)
+                return;
+
+            composer.CameraDistance = Mathf.Lerp(distances.x, distances.y, value01);
         }
     }
 }
