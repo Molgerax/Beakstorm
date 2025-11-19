8e2d6584e23a8bd7178834f6a0307e326b105ba6
Molgerax <molgerax.productions@gmail.com>
2025-11-19 10:17:45 +0100

    feat: added pheromone weapon pickup

---
+++ Assets/_Project/Scripts/Runtime/Gameplay/Player/Weapons/PheromoneWeaponPickup.cs
@@ -0,0 +1,37 @@
+﻿using TinyGoose.Tremble;
+using UnityEngine;
+
+namespace Beakstorm.Gameplay.Player.Weapons
+{
+    [PrefabEntity("weapon_pickup")]
+    public class PheromoneWeaponPickup : MonoBehaviour
+    {
+        [SerializeField] private PheromoneWeapon weapon;
+        [SerializeField, NoTremble] private SpriteRenderer spriteRenderer;
+
+
+        private void Awake()
+        {
+            if (spriteRenderer && weapon)
+                spriteRenderer.sprite = weapon.DisplaySprite;
+        }
+
+        private void Update()
+        {
+            if (spriteRenderer)
+                spriteRenderer.transform.Rotate(Vector3.up, 90 * Time.deltaTime);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!weapon)
+                return;
+        
+            if (other.TryGetComponent(out PheromoneWeaponInventory inventory))
+            {
+                inventory.GiveWeapon(weapon);
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
