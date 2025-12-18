1626709f691c2abddc1970a1e955add76823409f
Molgerax <molgerax.productions@gmail.com>
2025-12-18 16:08:41 +0100

    feat: first state machine setup

---
+++ Assets/_Project/Scripts/Runtime/Core/StateMachines/IState.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Beakstorm.Core.StateMachines
+{
+    public interface IState<T>
+    {
+        public void OnEnter(T entity);
+        public void OnExit(T entity);
+
+        public void OnUpdate(T entity, float deltaTime);
+        public void OnFixedUpdate(T entity, float deltaTime);
+    }
+}
