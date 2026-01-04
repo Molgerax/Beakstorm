07e03e6370d58062024281803403aff12157152a
Molgerax <molgerax.productions@gmail.com>
2026-01-04 19:58:32 +0100

    feat: added interface references & custom editors
    Interfaces can now be serialized by either using the "RequireInterface" attribute, or by using the "InterfaceReference" class

---
+++ Assets/_Project/Scripts/Runtime/Core/Interfaces/InterfaceReference.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Beakstorm.Core.Interfaces
+{
+    [Serializable]
+    public class InterfaceReference<TInterface, TObject> where TObject : Object where TInterface : class
+    {
+        [SerializeField, HideInInspector] private TObject underlyingValue;
+
+        public TInterface Value
+        {
+            get
+            {
+                if (underlyingValue is TInterface @interface)
+                    return @interface;
+                if (!underlyingValue)
+                    return null;
+                throw new InvalidOperationException(
+                    $"{underlyingValue} needs to implement interface {nameof(TInterface)}");
+            }
+            set
+            {
+                if (value is TObject newObject)
+                    underlyingValue = newObject;
+                else if (value == null)
+                    underlyingValue = null;
+                else
+                    throw new ArgumentException($"{value} needs to be of type {typeof(TObject)}.", String.Empty);
+            }
+        }
+
+        public TObject UnderlyingValue
+        {
+            get => underlyingValue;
+            set => underlyingValue = value;
+        }
+
+        public InterfaceReference() { }
+
+        public InterfaceReference(TObject target) => underlyingValue = target;
+
+        public InterfaceReference(TInterface @interface) => underlyingValue = @interface as TObject;
+        
+        public static implicit operator TInterface(InterfaceReference<TInterface, TObject> i) => i.Value;
+    }
+
+    [Serializable]
+    public class InterfaceReference<TInterface> : InterfaceReference<TInterface, Object> where TInterface : class
+    {
+        public static implicit operator TInterface(InterfaceReference<TInterface> i) => i.Value;
+    }
+}
