daa261ca3d8f8ab9c36ec5e134ed6f2d0e9b6f4d
Molgerax <molgerax.productions@gmail.com>
2026-01-10 21:13:48 +0100

    feat: random camera positioner

---
+++ Assets/_Project/Scripts/Runtime/Gameplay/Cameras/RandomCameraPositioner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Beakstorm.Gameplay.Cameras
+{
+    public class RandomCameraPositioner : MonoBehaviour
+    {
+        [SerializeField] private Transform target;
+        [SerializeField] private float distance = 500;
+
+        private void Update()
+        {
+            float dist = Vector3.Distance(target.position, transform.position);
+
+            if (dist > distance)
+            {
+                SetNewPosition();
+            }
+        }
+
+        public void SetNewPosition()
+        {
+            Vector3 newPos = target.position + target.forward * (distance * 0.85f) + Random.insideUnitSphere * (distance * 0.15f);
+            transform.position = newPos;
+        }
+    }
+}
