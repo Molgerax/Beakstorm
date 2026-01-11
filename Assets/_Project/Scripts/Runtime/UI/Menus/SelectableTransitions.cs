306ead0790378c1783d355554ff8bbe982b1112d
Molgerax <molgerax.productions@gmail.com>
2026-01-11 15:25:14 +0100

    feat: added Selectable Color profiles

---
+++ Assets/_Project/Scripts/Runtime/UI/Menus/SelectableTransitions.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Beakstorm.UI.Menus
+{
+    public class SelectableTransitions : MonoBehaviour
+    {
+        [SerializeField] private UITransitionProfile profile;
+
+        private void OnValidate()
+        {
+            Apply();
+        }
+
+        private void Awake()
+        {
+            Apply();
+        }
+
+        private void Apply()
+        {
+            if (profile && TryGetComponent(out Selectable selectable))
+                profile.Apply(selectable);
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Runtime/UI/Menus/UITransitionProfile.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Beakstorm.UI.Menus
+{
+    [CreateAssetMenu(fileName = "UITransitionProfile", menuName = "Beakstorm/UI/UI Transition Profile")]
+    public class UITransitionProfile : ScriptableObject
+    {
+        [SerializeField] private Selectable.Transition transition = Selectable.Transition.ColorTint;
+        [SerializeField] private ColorBlock colors;
+        [SerializeField] private SpriteState spriteState;
+
+        public void Apply(Selectable selectable)
+        {
+            selectable.transition = transition;
+            selectable.colors = colors;
+            selectable.spriteState = spriteState;
+        }
+    }
+}
