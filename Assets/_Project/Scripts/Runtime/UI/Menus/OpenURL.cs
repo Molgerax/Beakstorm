5d2aa59113f79bc0eb9d94e7c7e02be2a6e3d00a
Molgerax <molgerax.productions@gmail.com>
2026-01-11 15:25:53 +0100

    feat: added Wishlist button

---
+++ Assets/_Project/Scripts/Runtime/UI/Menus/OpenURL.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Beakstorm.UI.Menus
+{
+    public class OpenURL : MonoBehaviour
+    {
+        [SerializeField] private string url;
+
+        public void Open()
+        {
+            Application.OpenURL(url);
+        }
+    }
+}
