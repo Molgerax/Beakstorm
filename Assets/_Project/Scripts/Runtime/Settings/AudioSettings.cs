d335f2f34d715f333bf1fe3cac998fd0d38ec56a
Molgerax <molgerax.productions@gmail.com>
2026-01-18 14:44:26 +0100

    feat: new audio settings

--- Assets/_Project/Scripts/Runtime/Settings/AudioSettings.cs
+++ Assets/_Project/Scripts/Runtime/Settings/AudioSettings.cs
@@ -10,6 +10,7 @@ namespace Beakstorm.Settings
         public float MasterVolume = 0.5f;
         public float MusicVolume = 1f;
         public float SfxVolume = 1f;
+        public float BirdsVolume = 1f;
 
         public void SetMaster(float v)
         {
@@ -28,16 +29,24 @@ namespace Beakstorm.Settings
             SfxVolume = v;
             ApplySfx();
         }
+        
+        public void SetBirds(float v)
+        {
+            BirdsVolume = v;
+            ApplyBirds();
+        }
 
         public override void Apply()
         {
             ApplyMaster();
             ApplyMusic();
             ApplySfx();
+            ApplyBirds();
         }
         
         private void ApplyMaster() => AkUnitySoundEngine.SetRTPCValue("volume_master", MasterVolume);
         private void ApplyMusic() => AkUnitySoundEngine.SetRTPCValue("volume_music", MusicVolume);
         private void ApplySfx() => AkUnitySoundEngine.SetRTPCValue("volume_sfx", SfxVolume);
+        private void ApplyBirds() => AkUnitySoundEngine.SetRTPCValue("volume_birds", BirdsVolume);
     }
 }
