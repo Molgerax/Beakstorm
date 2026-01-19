f0c033793b4e1a92fe90160cfb4102187bbfc04b
Molgerax <molgerax.productions@gmail.com>
2026-01-20 00:22:02 +0100

    feat: adjusted shader includes

--- Assets/_Project/Shaders/Graphs/Subgraphs/GetMainLight.hlsl
+++ Assets/_Project/Shaders/Graphs/Subgraphs/GetMainLight.hlsl
@@ -27,13 +27,13 @@ float3 CalculateGlobalIllumination(Surface s)
     return s.bakedGI * DIM_FACTOR;
 }
 
-float3 CalculateCelShading(Light l, Surface s)
+float3 CalculateCelShading(Light l, Surface s, float step)
 {
-    float shadowAtten = linearstep(0, 0.1, l.shadowAttenuation);
-    float distanceAtten = linearstep(0, 0.1, l.distanceAttenuation);
+    float shadowAtten = saturate(linearstep(0, step, l.shadowAttenuation));// + 0.25);
+    float distanceAtten = linearstep(0, step, l.distanceAttenuation);
     float attenuation = shadowAtten * distanceAtten;
     float diffuse = saturate(dot(s.normal, l.direction));
-    diffuse = smoothstep(0, 0.1, diffuse);
+    diffuse = smoothstep(0, step, diffuse);
     diffuse *= attenuation;
     
     float3 h = SafeNormalize(l.direction + s.view);
@@ -48,7 +48,7 @@ float3 CalculateCelShading(Light l, Surface s)
 
 float3 CalculateLambertShading(Light l, Surface s)
 {
-    float shadowAtten = linearstep(0, 0.1, l.shadowAttenuation);
+    float shadowAtten = saturate(linearstep(0, 0.1, l.shadowAttenuation));// + 0.25);
     float distanceAtten = linearstep(0, 0.1, l.distanceAttenuation);
     float attenuation = shadowAtten * distanceAtten;
     float diffuse = saturate(dot(s.normal, l.direction));
@@ -74,7 +74,7 @@ void GetDimLight_float(out float3 Color)
 }
 
 
-void LightingCelShaded_float(float3 Position, float2 ScreenPosition, float3 Normal, float3 View, float Smoothness, out float3 Color)
+void LightingCelShaded_float(float3 Position, float2 ScreenPosition, float3 Normal, float3 View, float Smoothness, float Step, out float3 Color)
 {
 #ifndef SHADERGRAPH_PREVIEW
     Surface s = (Surface)0;
@@ -98,7 +98,7 @@ void LightingCelShaded_float(float3 Position, float2 ScreenPosition, float3 Norm
     Light light = GetMainLight(shadowCoord);
     MixRealtimeAndBakedGI(light, s.normal, s.bakedGI);
     Color = CalculateGlobalIllumination(s);
-    Color += CalculateCelShading(light, s);
+    Color += CalculateCelShading(light, s, Step);
 
     int pixelLightCount = GetAdditionalLightsCount();
     
@@ -112,7 +112,7 @@ void LightingCelShaded_float(float3 Position, float2 ScreenPosition, float3 Norm
     UNITY_LOOP for (uint lightIndex = 0; lightIndex < min(URP_FP_DIRECTIONAL_LIGHTS_COUNT, MAX_VISIBLE_LIGHTS); lightIndex++)
     {
         Light additionalLight = GetAdditionalLight(lightIndex, inputData.positionWS, half4(1,1,1,1));
-        Color += CalculateCelShading(additionalLight, s);
+        Color += CalculateCelShading(additionalLight, s, Step);
     }
     #endif
     
@@ -121,7 +121,7 @@ void LightingCelShaded_float(float3 Position, float2 ScreenPosition, float3 Norm
             lightIndex = GetPerObjectLightIndex(lightIndex);
         #endif
         light = GetAdditionalLight(lightIndex, Position, 1);
-        Color += saturate(CalculateCelShading(light, s));
+        Color += saturate(CalculateCelShading(light, s, Step));
     LIGHT_LOOP_END
     
     
@@ -149,6 +149,38 @@ void MainLight_float(out float3 Direction, out float3 Color, out float DistanceA
 #endif
 }
 
+void GlobalIllumination_float(float3 Position, float3 Normal, float3 View, float Smoothness, out float3 Color)
+{
+#ifndef SHADERGRAPH_PREVIEW
+    Surface s = (Surface)0;
+    s.normal = SafeNormalize(Normal);
+    s.view = SafeNormalize(View);
+    s.shininess = exp2(10 * Smoothness + 1);
+    s.smoothness = Smoothness;
+
+    float3 vertexSH;
+    OUTPUT_SH(Normal, vertexSH);
+    // This function calculates the final baked lighting from light maps or probes
+    s.bakedGI = SAMPLE_GI(lightmapUV, vertexSH, Normal);
+    
+    #if SHADOWS_SCREEN
+    float4 clipPos = TransformWorldToHClip(Position);
+    float4 shadowCoord = ComputeScreenPos(clipPos);
+    #else
+    float4 shadowCoord = TransformWorldToShadowCoord(Position);
+    #endif
+
+    Light light = GetMainLight(shadowCoord);
+    MixRealtimeAndBakedGI(light, s.normal, s.bakedGI);
+    Color = CalculateGlobalIllumination(s);
+
+#else
+    Color = 1;
+#endif    
+}
+
+
+
 
 
 void LightingLambertShaded_float(float3 Position, float2 ScreenPosition, float3 Normal, float3 View, float Smoothness, out float3 Color)
@@ -173,6 +205,64 @@ void LightingLambertShaded_float(float3 Position, float2 ScreenPosition, float3 
     #endif
 
     Light light = GetMainLight(shadowCoord);
+    MixRealtimeAndBakedGI(light, s.normal, s.bakedGI);
+    Color = CalculateGlobalIllumination(s);
+    Color += CalculateLambertShading(light, s);
+
+    int pixelLightCount = GetAdditionalLightsCount();
+    
+    #if USE_CLUSTER_LIGHT_LOOP
+    InputData inputData = (InputData)0;
+    inputData.normalizedScreenSpaceUV = ScreenPosition;
+    inputData.positionWS = Position;
+    #endif
+
+    #if USE_CLUSTER_LIGHT_LOOP
+    UNITY_LOOP for (uint lightIndex = 0; lightIndex < min(URP_FP_DIRECTIONAL_LIGHTS_COUNT, MAX_VISIBLE_LIGHTS); lightIndex++)
+    {
+        Light additionalLight = GetAdditionalLight(lightIndex, inputData.positionWS, half4(1,1,1,1));
+        Color += CalculateLambertShading(additionalLight, s);
+    }
+    #endif
+    
+    LIGHT_LOOP_BEGIN(pixelLightCount)
+        #if !USE_CLUSTER_LIGHT_LOOP
+            lightIndex = GetPerObjectLightIndex(lightIndex);
+    #endif
+    light = GetAdditionalLight(lightIndex, Position, 1);
+    Color += saturate(CalculateLambertShading(light, s));
+    LIGHT_LOOP_END
+    
+    
+#else
+    Color = 1;
+    #endif    
+}
+
+
+void LightingLambert_NoShadow_float(float3 Position, float2 ScreenPosition, float3 Normal, float3 View, float Smoothness, out float3 Color)
+{
+    #ifndef SHADERGRAPH_PREVIEW
+    Surface s = (Surface)0;
+    s.normal = SafeNormalize(Normal);
+    s.view = SafeNormalize(View);
+    s.shininess = exp2(10 * Smoothness + 1);
+    s.smoothness = Smoothness;
+
+    float3 vertexSH;
+    OUTPUT_SH(Normal, vertexSH);
+    // This function calculates the final baked lighting from light maps or probes
+    s.bakedGI = SAMPLE_GI(lightmapUV, vertexSH, Normal);
+    
+    #if SHADOWS_SCREEN
+    float4 clipPos = TransformWorldToHClip(Position);
+    float4 shadowCoord = ComputeScreenPos(clipPos);
+    #else
+    float4 shadowCoord = TransformWorldToShadowCoord(Position);
+    #endif
+
+    Light light = GetMainLight(shadowCoord);
+    light.shadowAttenuation = 1;
     MixRealtimeAndBakedGI(light, s.normal, s.bakedGI);
     Color = CalculateGlobalIllumination(s);
     Color += CalculateLambertShading(light, s);
