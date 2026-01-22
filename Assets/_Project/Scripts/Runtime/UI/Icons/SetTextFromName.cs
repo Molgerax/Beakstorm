55941292b915c1ea6801972cae3f2d69c248c9ff
Molgerax <molgerax.productions@gmail.com>
2026-01-22 13:01:42 +0100

    feat: added controls screen

--- Assets/_Project/Scripts/Runtime/UI/Icons/SetTextFromName.cs
+++ Assets/_Project/Scripts/Runtime/UI/Icons/SetTextFromName.cs
@@ -1,9 +1,27 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 namespace Beakstorm.UI.Icons
 {
     public class SetTextFromName : MonoBehaviour
     {
-        
+        [SerializeField] private TMP_Text text;
+        [SerializeField] private string prefix = "Player/";
+
+        private void Awake()
+        {
+            SetText();
+        }
+
+        private void OnValidate()
+        {
+            SetText();
+        }
+
+        private void SetText()
+        {
+            if (text)
+                text.text = @$"{{{prefix}{gameObject.name}}}";
+        }
     }
 }
