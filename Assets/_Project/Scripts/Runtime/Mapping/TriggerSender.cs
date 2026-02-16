04f18a7c8eb109b509e8ef638f63bb4684eb60b9
Molgerax <molgerax.productions@gmail.com>
2026-02-17 00:54:18 +0100

    feat: gizmos

--- Assets/_Project/Scripts/Runtime/Mapping/TriggerSender.cs
+++ Assets/_Project/Scripts/Runtime/Mapping/TriggerSender.cs
@@ -26,5 +26,55 @@ namespace Beakstorm.Mapping
             targets = _targets.TriggerToComponent();
             invertSignal = _invertSignal;
         }
+
+
+        protected virtual void OnDrawGizmos()
+        {
+            Gizmos.color = new(0, 1, 0, 0.5f);
+
+            foreach (Component target in targets)
+            {
+                DrawGizmoArrow(transform.position, target.transform.position);
+            }
+        }
+        
+        protected virtual void OnDrawGizmosSelected()
+        {
+            Gizmos.color = new(1, 0, 0);
+
+            foreach (Component target in targets)
+            {
+                DrawGizmoArrow(transform.position, target.transform.position);
+            }
+        }
+
+        protected void DrawGizmoArrow(Vector3 startPos, Vector3 endPos)
+        {
+            Gizmos.DrawLine(startPos, endPos);
+            
+            float percentage = 0.75f;
+            Vector3 diff = endPos - startPos;
+            Vector3 arrowBase = startPos + diff * percentage;
+
+            
+            DrawGizmoArrowHead(arrowBase, diff.normalized);
+        }
+
+        protected void DrawGizmoArrowHead(Vector3 pos, Vector3 dir)
+        {
+            int count = 3;
+            float length = 3f;
+
+            Vector3 arrowDirection = -dir.normalized;
+            Vector3 right = Vector3.Cross(Vector3.up, dir).normalized;
+            arrowDirection = Quaternion.AngleAxis(22.5f, right) * arrowDirection;
+
+            for (int i = 0; i < count; i++)
+            {
+                float degrees = 360f / count;
+                Gizmos.DrawLine(pos, pos + arrowDirection * length);
+                arrowDirection = Quaternion.AngleAxis(degrees, dir.normalized) * arrowDirection;
+            }
+        }
     }
 }
