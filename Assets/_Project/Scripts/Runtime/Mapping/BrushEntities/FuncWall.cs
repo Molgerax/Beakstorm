6039465f3bf0f0751c68c64227fa14bb362e32f6
Molgerax <molgerax.productions@gmail.com>
2026-02-17 18:08:39 +0100

    feat: added simple wall brush

---
+++ Assets/_Project/Scripts/Runtime/Mapping/BrushEntities/FuncWall.cs
@@ -0,0 +1,11 @@
+﻿using TinyGoose.Tremble;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace Beakstorm.Mapping.BrushEntities
+{
+    [BrushEntity("wall", category:"func", BrushType.Solid)]
+    public class FuncWall : MonoBehaviour
+    {
+    }
+}
