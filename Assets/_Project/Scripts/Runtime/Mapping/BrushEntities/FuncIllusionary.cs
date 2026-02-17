b43b697f0cac6ec2c8a092cc0d128bf67c42e7c7
Molgerax <molgerax.productions@gmail.com>
2026-02-17 11:36:53 +0100

    feat: added some tooling funcs

---
+++ Assets/_Project/Scripts/Runtime/Mapping/BrushEntities/FuncIllusionary.cs
@@ -0,0 +1,21 @@
+﻿using TinyGoose.Tremble;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace Beakstorm.Mapping.BrushEntities
+{
+    [BrushEntity("illusionary", category:"func")]
+    public class FuncIllusionary : MonoBehaviour, IOnImportFromMapEntity
+    {
+        [Tremble, SpawnFlags()] private bool _castShadows = false;
+        
+        public void OnImportFromMapEntity(MapBsp mapBsp, BspEntity entity)
+        {
+            MeshCollider meshCollider = GetComponent<MeshCollider>();
+            CoreUtils.Destroy(meshCollider);
+
+            MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+            meshRenderer.shadowCastingMode = _castShadows ? ShadowCastingMode.On : ShadowCastingMode.Off;
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Runtime/Mapping/BrushEntities/FuncLiquid.cs
@@ -0,0 +1,18 @@
+﻿using TinyGoose.Tremble;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace Beakstorm.Mapping.BrushEntities
+{
+    [BrushEntity("liquid", category:"func", BrushType.Liquid)]
+    public class FuncLiquid : MonoBehaviour, IOnImportFromMapEntity
+    { 
+        [Tremble, SpawnFlags()] private bool _castShadows = false;
+
+        public void OnImportFromMapEntity(MapBsp mapBsp, BspEntity entity)
+        {
+            MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+            meshRenderer.shadowCastingMode = _castShadows ? ShadowCastingMode.On : ShadowCastingMode.Off;
+        }
+    }
+}
